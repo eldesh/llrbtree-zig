d1f8fd35c67bc09a08064a3b8ef546df4151c873
eldesh <nephits@gmail.com>
2022-09-04 21:24:08 +0900

    zigmod init

--- build.zig
+++ build.zig
@@ -1,16 +1,19 @@
 const std = @import("std");
+const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("rbtree-zig.git", "src/main.zig");
+    const lib = b.addStaticLibrary("rbtree-zig", "src/main.zig");
     lib.setBuildMode(mode);
+    deps.addAllTo(lib);
     lib.install();
 
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
+    deps.addAllTo(main_tests);
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
