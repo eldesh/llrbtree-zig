499c98d7b892e6d1a785897976687c7adedbeceb
eldesh <nephits@gmail.com>
2022-09-06 23:40:49 +0900

    Add: deleteMin

--- src/main.zig
+++ src/main.zig
@@ -11,6 +11,7 @@ const std = @import("std");
 const Con = @import("basis_concept");
 
 const testing = std.testing;
+const math = std.math;
 
 const Allocator = std.mem.Allocator;
 
@@ -44,9 +45,12 @@ pub fn LLRBTreeSet(comptime T: type) type {
         root: ?*Node,
 
         pub const Node = struct {
+            // color of the incoming link (from parent)
             color: NodeColor,
             value: T,
+            // left child node
             lnode: ?*Node,
+            // right child node
             rnode: ?*Node,
 
             fn new(alloc: Allocator, value: T, lnode: ?*Node, rnode: ?*Node) !*Node {
@@ -89,9 +93,20 @@ pub fn LLRBTreeSet(comptime T: type) type {
                 self.rnode.?.color.flip();
             }
 
+            fn fixup(self: *Node) *Node {
+                var h = self;
+                if (isRed(h.rnode))
+                    h = h.rotate_left();
+                if (isRed(h.lnode) and isRed(h.lnode.?.lnode))
+                    h = h.rotate_right();
+                if (isRed(h.lnode) and isRed(h.rnode))
+                    h.flip_color();
+                return h;
+            }
+
             fn insert_node(self: ?*Node, allocator: Allocator, t: T) Allocator.Error!*Node {
                 if (self) |node| {
-                    if (Node.isRed(node.lnode) and isRed(node.rnode)) node.flip_color();
+                    if (isRed(node.lnode) and isRed(node.rnode)) node.flip_color();
 
                     switch (Con.PartialOrd.on(*const T)(&node.value, &t).?) {
                         .lt => node.lnode = try insert_node(node.lnode, allocator, t),
@@ -110,8 +125,60 @@ pub fn LLRBTreeSet(comptime T: type) type {
                 }
             }
 
+            // Checks if node `self` is not null and the value of the color field is equal to `.Red`.
             fn isRed(self: ?*Node) bool {
                 return if (self) |node| node.color == .Red else false;
+            }
+
+            //
+            fn move_redleft(self: *Node) *Node {
+                var h = self;
+                h.flip_color();
+                if (self.rnode != null and isRed(self.rnode.?.lnode)) {
+                    h.rnode = h.rnode.?.rotate_right();
+                    h = h.rotate_left();
+                    h.flip_color();
+                }
+                return h;
+            }
+
+            fn move_redright(self: *Node) *Node {
+                var h = self;
+                h.flip_color();
+                if (self.lnode != null and isRed(self.lnode.?.lnode)) {
+                    h = h.rotate_right();
+                    h.flip_color();
+                }
+                return h;
+            }
+
+            fn delete_node(self: *Node, value: *const T) *Node {
+                var h = self;
+                if (Con.PartialOrd.on(*const T)(value, &h.value).?.compare(math.CompareOperator.lt)) {
+                    if (!isRed(h.lnode) and h.lnode != null and !isRed(h.lnode.?.lnode))
+                        h = h.move_redleft();
+                    h.lnode = delete_node(h.lnode.?, value);
+                } else {
+                    unreachable;
+                }
+                return h.fixup();
+            }
+
+            fn delete_min_node(self: ?*Node, allocator: Allocator) ?*Node {
+                if (self == null)
+                    return null;
+
+                if (self.?.lnode == null) {
+                    allocator.destroy(self.?);
+                    return null;
+                }
+
+                var h = self.?;
+                if (!isRed(h.lnode) and !isRed(h.lnode.?.lnode))
+                    h = h.move_redleft();
+
+                h.lnode = delete_min_node(h.lnode, allocator);
+                return h.fixup();
             }
         };
 
@@ -147,8 +214,19 @@ pub fn LLRBTreeSet(comptime T: type) type {
             self.root.?.color = .Black;
         }
 
-        // pub fn remove(self: *Self, key: *K) ?Value {
-        // }
+        pub fn delete(self: *Self, value: *const T) void {
+            if (self.root) |root| {
+                self.root = root.delete_node(value);
+                if (self.root) |_|
+                    self.root.?.color = .Black;
+            }
+        }
+
+        pub fn delete_min(self: *Self) void {
+            self.root = Node.delete_min_node(self.root, self.allocator);
+            if (self.root) |root|
+                root.color = .Black;
+        }
 
         // pub fn get(self: *Self, key: *K) ?Entry {
         // }
@@ -160,16 +238,20 @@ pub fn LLRBTreeSet(comptime T: type) type {
 
 test "LLRBTreeSet" {
     const Tree = LLRBTreeSet(u32);
-    var tree = Tree.new(testing.allocator);
-    defer tree.destroy();
-    try tree.insert(1);
-    try tree.insert(2);
-    try tree.insert(3);
-    try tree.insert(4);
-    try tree.insert(5);
-    try tree.insert(5);
-    try tree.insert(4);
-    try tree.insert(3);
-    try tree.insert(2);
-    try tree.insert(1);
+    {
+        var tree = Tree.new(testing.allocator);
+        defer tree.destroy();
+        var i: u32 = 0;
+        while (i <= 5) : (i += 1)
+            try tree.insert(i);
+        while (i > 0) : (i -= 1)
+            try tree.insert(i);
+
+        i = 0;
+        while (i < 10) : (i += 1)
+            tree.delete_min();
+    }
+
+    // var val: u32 = 1;
+    // tree.delete_min(&val);
 }
